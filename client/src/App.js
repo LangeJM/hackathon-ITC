530bc29c9d35f04cc663cfa0ca5a1a7a2276dba5
Aaron Lieblich <lieblichaaron@gmail.com>
2021-01-20 17:16:32 +0200

    create map

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import VaccData from "./BottomLeft/VaccData";
 import TitleBanner from "./TitleBanner/titleBanner";
 import "bootstrap/dist/css/bootstrap.min.css";
+import MainMap from "./Map/map";
 
 function App() {
   return (
     <div className="App">
       <TitleBanner />
+      <MainMap />
       <VaccData />
     </div>
   );
--- client/src/Map/map.jsx
+++ client/src/Map/map.jsx
@@ -1,5 +1,14 @@
+import ReactTooltip from "react-tooltip";
+import React, { useState } from "react";
+import MapChart from "./mapChart";
 const MainMap = () => {
-  return null;
+  const [content, setContent] = useState("");
+  return (
+    <div>
+      <MapChart setTooltipContent={setContent} />
+      <ReactTooltip>{content}</ReactTooltip>
+    </div>
+  );
 };
 
 export default MainMap;
