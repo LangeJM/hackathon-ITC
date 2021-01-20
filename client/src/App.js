13e2e3de43546cf14f287f1a98a9f91f5e39107b
Ruti Zwick <rutizwick@gmail.com>
2021-01-20 17:01:23 +0200

    recent data card

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,17 @@
 import "./App.css";
 import RecentData from "./BottomLeft/RecentData";
 import VaccData from "./BottomLeft/VaccData";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
+import TitleBanner from "./TitleBanner/titleBanner";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App">
-    <VaccData/>
-    <RecentData/>
-  </div>;
+  return (
+    <div className="App">
+      <TitleBanner />
+      <VaccData />
+      <RecentData/>
+    </div>
+  );
 }
 
 export default App;
