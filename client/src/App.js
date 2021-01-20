1685f53861559f48f0ea0f5bdfe93a98ab3a16a8
Ruti Zwick <rutizwick@gmail.com>
2021-01-20 16:53:35 +0200

    recent data

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import RecentData from "./BottomLeft/RecentData";
 import VaccData from "./BottomLeft/VaccData";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
   return <div className="App">
     <VaccData/>
+    <RecentData/>
   </div>;
 }
 
