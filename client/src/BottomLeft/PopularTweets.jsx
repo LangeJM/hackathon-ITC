5b1d5640975824db144333754aba84f6f81be644
Aaron Lieblich <lieblichaaron@gmail.com>
2021-01-21 10:54:49 +0200

    tweets flex column

--- client/src/BottomLeft/PopularTweets.jsx
+++ client/src/BottomLeft/PopularTweets.jsx
@@ -1,34 +1,31 @@
-import React, { useState } from 'react'
-import Card from 'react-bootstrap/Card'
-import { TwitterTweetEmbed } from 'react-twitter-embed'
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import { TwitterTweetEmbed } from "react-twitter-embed";
 
 export default function PopularTweets() {
-
-  const [tweet1, setTweet1] = useState('1351920219321729024')
-  const [tweet2, setTweet2] = useState('1351907570236190720')
-  const [tweet3, setTweet3] = useState('1352148443322605568')
+  const [tweet1, setTweet1] = useState("1351920219321729024");
+  const [tweet2, setTweet2] = useState("1351907570236190720");
+  const [tweet3, setTweet3] = useState("1352148443322605568");
 
   return (
     <Card
-      border='light'
+      border="light"
       style={{
-        width: '90%',
-        backgroundColor: '#D9CDB8',
-        padding: '0.5rem',
-        margin: '1rem auto'
+        width: "90%",
+        backgroundColor: "#D9CDB8",
+        padding: "0.5rem",
+        margin: "1rem auto",
       }}
-      className='text-center'
+      className="text-center"
     >
-      <Card.Header style={{ fontWeight: 'bold', backgroundColor: 'white' }}>
+      <Card.Header style={{ fontWeight: "bold", backgroundColor: "white" }}>
         Popular Tweets about the Vaccine
       </Card.Header>
-      <div 
-      style={{justifyContent: 'space-evenly'}}
-      className='d-flex'>
+      <div style={{ justifyContent: "space-evenly" }} className="d-flex-column">
         <TwitterTweetEmbed tweetId={tweet1} />
         <TwitterTweetEmbed tweetId={tweet2} />
         <TwitterTweetEmbed tweetId={tweet3} />
       </div>
     </Card>
-  )
+  );
 }
